6bd5daa77be32933abfcc95bc0ebcea48a85ecc3
Alex Babenko <bob78alex@gmail.com>
2019-04-08 20:02:29 -0400

    Setting up environment, + basic template

--- src/App.js
+++ src/App.js
@@ -1,28 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Form, Field } from 'react-final-form';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+const showResults = async values => {
+  await sleep(500) // to simulate server delay
+  console.log(JSON.stringify(values, undefined, 2))
 }
 
+const App = () => (
+  <div>
+  <h1> Congratulations! </h1>
+   <p>
+          You have asnwered the question correctly. Please fill in your 
+          mailing address and phone number so we ship you your prize 
+  </p>
+
+          <Form onSubmit = {showResults}>
+          {({handleSubmit}) => (
+          <form onSubmit={handleSubmit}>
+         
+         <div>
+            <label>First Name</label>
+            <Field name="firstName" component="input" />
+         </div>
+          <div>
+            <label>Last Name</label>
+            <Field 
+            name="lastName" component="input" /*placeholder="Last Name"*//>
+         </div>
+         <div>
+            <label>Address</label>
+            <Field 
+            name="addressLine1" component="input" /*placeholder="Address"*//>
+         </div>
+         <div>
+            <label>Address Line 2</label>
+            <Field 
+            name="addressLine2" component="input" placeholder="Apartment, suite, unit, building, floor etc"/>
+         </div>
+         <div>
+            <label>Address type</label>
+            <select
+            name="addressType" component="select" placeholder="Residential">
+            <option value="residential">Residential</option>
+            <option value="municipal">Municipal</option>
+            <option value="business">Business</option>
+            </select>
+         </div>
+         <div>
+            <label>City</label>
+            <Field 
+            name="city" component="input" />
+         </div>
+         <div>
+            <label>Province</label>
+            <Field 
+            name="province" component="input" />
+         </div>
+          <div>
+            <label>Postal code</label>
+            <Field 
+            name="postalCode" component="input" />
+              </div>
+               <div>
+            <label>Phone number</label>
+            <Field 
+            name="phoneNumber" component="input" />
+         </div>
+         <button type="submit">Submit</button>
+
+         </form>
+         
+         )}
+
+         </Form>
+     </div>
+         )
+
+
 export default App;
+
+         
+
+
